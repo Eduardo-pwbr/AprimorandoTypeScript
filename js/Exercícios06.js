86827ebf6ad012d149ebca89104d1840b790d633
Eduardo-pwbr <eduardo.f.o.rodrigues@hotmail.com>
2026-01-23 04:13:36 -0300

    feat:including new verification instanceof

---
+++ js/Exercícios06.js
@@ -0,0 +1,7 @@
+"use strict";
+const link = document.getElementById("origamid");
+if (link instanceof HTMLAnchorElement) {
+    console.log(link.href);
+    link.href = link.href.replace("http://", "https://");
+}
+console.log(link);
---
+++ js/Exercícios06.ts
@@ -0,0 +1,8 @@
+const link = document.getElementById("origamid");
+
+if (link instanceof HTMLAnchorElement) {
+  console.log(link.href);
+  link.href = link.href.replace("http://", "https://");
+
+}
+console.log(link);
