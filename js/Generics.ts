f4fdede715de293b0d48f8b1cd71d32f01b3e75d
Eduardo-pwbr <eduardo.f.o.rodrigues@hotmail.com>
2026-01-24 22:02:55 -0300

    feat:including new features about generics

---
+++ js/Generics.ts
@@ -0,0 +1,72 @@
+/*function retornoGenerics<variavel>(a:variavel): variavel {
+    return a;
+}*/
+/*function retornoGenerics<string>(a: string): string {
+    return a;
+}
+function retornoGenerics<number>(a: number): number {
+    return a;
+}
+function retornoGenerics<boolean>(a: boolean): boolean {
+    return a;
+}*/
+
+// Testando a função com diferentes tipos de dados usando Generics
+//ela pode retornar qualquer tipo de dado, só é necessário informar o tipo ao chamar a função
+
+/*console.log(retornoGenerics<string>("Olá Mundo"));
+console.log(retornoGenerics(1234));
+console.log(retornoGenerics(true));*/
+
+const  listanumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const  frutas = ['Banana', 'Maçã', 'Laranja', 'Uva', 'Abacaxi', 'Manga','Morango']; 
+
+const listaValores = ['Banana',1,'morango',2,'pera',3,'jabuticaba',4,'melão']
+
+/*function retornarArray<tipo>(arr: tipo[]): tipo[] {
+    return arr;
+}*/
+
+
+function retorno<tipo>(lista:tipo[]):tipo[]{
+  return lista.slice(0, 5)
+}
+
+function retornalista<T>(lista:T[]):T[]{
+  return lista.slice(0,4)
+}
+
+//console.log(retornarArray<number>(numeros));
+//console.log(retornarArray<string>(frutas));
+
+console.log(retorno(listanumeros));
+console.log(retorno(frutas));
+console.log(retornalista(listaValores));
+console.log(retornalista(listaValores).map(item => item));
+
+
+//outro exemplo de generics
+
+function notnull<T>(arg:T)  {
+
+    if(arg !== null){return arg;
+    }
+    else{
+        return  null;
+    }
+    
+}
+notnull("Eduardo")?.toLowerCase();
+notnull(200)?.toFixed();
+
+
+function tipoDados<T>(arg:T) {
+    const resultado = { dados: arg, tipo: typeof arg };
+    console.log(resultado)
+    return typeof(arg);
+}
+tipoDados(true);
+tipoDados(1234);
+tipoDados("Teste");
+tipoDados([1,2,3]);
